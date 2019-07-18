4d6f4077eae7c155fdcdf0c7951657fd8c2b4d7b
cqfee <cqfeetop@163.com>
2019-07-18 18:30:18 +0800

    furniture

--- lib/pages/home_page/detailData.dart
+++ lib/pages/home_page/detailData.dart
@@ -2,19 +2,25 @@ import 'package:flutter/material.dart';
 
 var furnitureDetailList = [
   ItemModel(
-    imageUrl: 'assets/images/furniture_detail_01.jpeg',
+    imageUrl: 'assets/images/f1.jpeg',
     title: 'Hemes ArmChair',
     subTitle: 'NEW SALE',
     price: '128',
   ),
    ItemModel(
-    imageUrl: 'assets/images/furniture_detail_02.jpeg',
+    imageUrl: 'assets/images/f2.jpeg',
     title: 'Sofar ArmChari',
     subTitle: 'OLD SALE',
     price: '328',
   ),
    ItemModel(
-    imageUrl: 'assets/images/furniture_detail_03.jpeg',
+    imageUrl: 'assets/images/f3.jpeg',
+    title: 'Wodden ArmChair',
+    subTitle: 'FRESH SALE',
+    price: '628',
+  ),
+  ItemModel(
+    imageUrl: 'assets/images/f4.jpeg',
     title: 'Wodden ArmChair',
     subTitle: 'FRESH SALE',
     price: '628',
