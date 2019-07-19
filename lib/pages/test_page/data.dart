59402719c7ff6df67e080abe148f0e5e9076f722
cqfee <cqfeetop@163.com>
2019-07-19 14:11:44 +0800

    :alembic: pageview

---
+++ lib/pages/test_page/data.dart
@@ -0,0 +1,34 @@
+import'package:flutter/material.dart';
+
+class ItemModel {
+  String imageUrl;
+  String title;
+  Color color;
+  ItemModel({this.imageUrl, this.title, this.color});
+}
+
+var lists = [
+  ItemModel(
+    imageUrl: 'assets/images/f01.png',
+    title: 'Beautiful 01',
+    color: Colors.blue,
+  ),
+  ItemModel(
+    imageUrl: 'assets/images/f02.png',
+    title: 'Beautiful 02',
+    color: Colors.lightGreen,
+  ),
+  ItemModel(
+    imageUrl: 'assets/images/f03.png',
+    title: 'Beautiful 03',
+    color: Colors.deepPurple,
+  ),
+  ItemModel(
+    imageUrl: 'assets/images/furniture_detail_01.jpeg',
+    title: 'Beautiful 04',
+  ),
+  ItemModel(
+    imageUrl: 'assets/images/furniture_detail_02.jpeg',
+    title: 'Beautiful 05',
+  ),
+];
--- lib/pages/test_page/stack_test.dart
+++ lib/pages/test_page/stack_test.dart
@@ -1,55 +1,80 @@
 import 'package:flutter/material.dart';
+import 'data.dart';
 
 class StackTestPage extends StatelessWidget {
+  Widget _item(item) {
+    return Container(
+      width:300,
+      height:20,
+      decoration: BoxDecoration(
+        color: item.color
+      ),
+    );
+  }
+  _pageList() {
+    List<Widget> _list = [];
+    for(var i = 0; i < lists.length; i++) {
+      _list.add(_item(lists[i]));
+    }
+    return _list;
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       backgroundColor: Colors.white,
-      body: Center(
-        child: SizedBox(
-          height: 300.0,
-          width: 200.0,
-          child: Stack(
-            fit: StackFit.expand,
-            overflow: Overflow.visible,
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.deepPurple,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black12,
-                        offset: Offset(0.0, 12.0),
-                        blurRadius: 10.0,
-                      )
-                    ]),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    SizedBox(
-                      height: 20,
-                      child:  Container(
-                          decoration: BoxDecoration(color: Colors.blue),
-                          child: Text('1234')),
-                    ),
-                    Text('你打发放付多过过过过'),
-                  ],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          ..._pageList(),
+          SizedBox(
+            height: 300.0,
+            width: 200.0,
+            child: Stack(
+              fit: StackFit.expand,
+              overflow: Overflow.visible,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          offset: Offset(0.0, 12.0),
+                          blurRadius: 10.0,
+                        )
+                      ]),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 20,
+                        child:  Container(
+                            decoration: BoxDecoration(color: Colors.blue),
+                            child: Text('1234')),
+                      ),
+                      Text('你打发放付多过过过过'),
+                    ],
+                  ),
                 ),
-              ),
-              Positioned(
-                  top: -80,
-                  left: 0,
-                  right: 0,
-                  child: Image.asset(
-                    'assets/images/furniture_01.png',
-                    width: 50,
-                  )),
-            ],
+                Positioned(
+                    top: -80,
+                    left: 0,
+                    right: 0,
+                    child: Image.asset(
+                      'assets/images/furniture_01.png',
+                      width: 50,
+                    )),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
